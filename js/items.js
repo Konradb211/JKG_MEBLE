6d0e9219e86c88d00192351738192252af6dc8a6
Konrad Baś <konradbas@192.168.1.8>
2023-07-31 22:56:58 +0200

    items sections

--- js/items.js
+++ js/items.js
@@ -1,36 +1,18 @@
 const navMobile = document.querySelector(".navbar-mobile")
 const navBtn = document.querySelector(".hamburger")
-const errorText = document.querySelector(".errorMail")
-const errorArea = document.querySelector(".errorArea")
-const email = document.querySelector(".text")
-const formBtn = document.querySelector(".contact__form-btnBox")
-const textarea = document.querySelector(".textarea")
 const footerYear = document.querySelector(".footeryear")
 const allLinks = document.querySelectorAll(".navbar-mobile__link")
-
-const formFunction = () => {
-	checkMail()
-	checkTextarea()
-}
+const beds = document.querySelectorAll(".beds")
+const kitchenCorners = document.querySelectorAll(".kitchenCorners")
+const corners = document.querySelectorAll(".corners")
+const pouffe = document.querySelector(".pouffe")
+const list = document.querySelector(".list")
 
 const checkTextarea = () => {
 	if (textarea.value === "") {
 		errorArea.style.visibility = "visible"
 	} else {
 		errorArea.style.visibility = "hidden"
-	}
-}
-
-const checkMail = () => {
-	const re =
-		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-	if (re.test(email.value)) {
-		errorText.style.visibility = "hidden"
-	} else if (email.value === "") {
-		errorText.style.visibility = "visible"
-	} else {
-		errorText.style.visibility = "visible"
 	}
 }
 
@@ -58,6 +40,5 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year
 }
 
-formBtn.addEventListener("click", formFunction)
 navBtn.addEventListener("click", handleNav)
 handleCurrentYear()
