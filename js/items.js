7099155560b1eb346c2a040d3f9df04d9735c628
Konrad Baś <konradbas@192.168.1.8>
2023-08-07 20:58:35 +0200

    img scaling

--- js/items.js
+++ js/items.js
@@ -7,6 +7,18 @@ const kitchenCorners = document.querySelectorAll(".kitchenCorners")
 const corners = document.querySelectorAll(".corners")
 const pouffe = document.querySelector(".pouffe")
 const list = document.querySelector(".list")
+const btnMon = document.querySelector(".monolith")
+const btnInari = document.querySelector(".inari")
+const btnSoft = document.querySelector(".soft")
+const btnSoftTwo = document.querySelector(".softTwo")
+const imgButtons = document.querySelector(".buttons")
+const btnX = document.querySelector(".buttons__icon")
+const monoImg = document.querySelector(".monolitImg")
+const inariImg = document.querySelector(".inariImg")
+const softImg = document.querySelector(".softImg")
+const soft2Img = document.querySelector(".soft2Img")
+const html = document.querySelector("html")
+const itemPhoto = document.querySelector(".photo")
 
 const checkTextarea = () => {
 	if (textarea.value === "") {
@@ -35,6 +47,66 @@ const handleNav = () => {
 	)
 }
 
+const zoomImg = e => {
+	const x = e.clientX
+	const y = e.clientY
+
+	const imgX = itemPhoto.offsetLeft
+	const imgY = itemPhoto.offsetTop
+
+	const newX = (imgX - x) * -1
+	const newY = (imgY - y) * -1
+
+	itemPhoto.style.transformOrigin = `${newX}px ${newY}px`
+
+	itemPhoto.classList.add("zoom-img")
+}
+
+const resetImg = () => {
+	itemPhoto.classList.remove("zoom-img")
+}
+
+const showMon = () => {
+	imgButtons.style.display = "flex"
+	monoImg.style.display = "block"
+	inariImg.style.display = "none"
+	softImg.style.display = "none"
+	soft2Img.style.display = "none"
+	document.getElementsByTagName("body")[0].style.overflow = "hidden"
+	html.style.scrollBehavior = "auto"
+}
+const showInari = () => {
+	imgButtons.style.display = "flex"
+	monoImg.style.display = "none"
+	inariImg.style.display = "block"
+	softImg.style.display = "none"
+	soft2Img.style.display = "none"
+	document.getElementsByTagName("body")[0].style.overflow = "hidden"
+	html.style.scrollBehavior = "auto"
+}
+const showSoft = () => {
+	imgButtons.style.display = "flex"
+	monoImg.style.display = "none"
+	inariImg.style.display = "none"
+	softImg.style.display = "block"
+	soft2Img.style.display = "none"
+	document.getElementsByTagName("body")[0].style.overflow = "hidden"
+	html.style.scrollBehavior = "auto"
+}
+const showSoft2 = () => {
+	imgButtons.style.display = "flex"
+	monoImg.style.display = "none"
+	inariImg.style.display = "none"
+	softImg.style.display = "none"
+	soft2Img.style.display = "block"
+	document.getElementsByTagName("body")[0].style.overflow = "hidden"
+	html.style.scrollBehavior = "auto"
+}
+
+const removePicture = () => {
+	imgButtons.style.display = "none"
+	document.getElementsByTagName("body")[0].style.overflow = "visible"
+}
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
@@ -42,3 +114,10 @@ const handleCurrentYear = () => {
 
 navBtn.addEventListener("click", handleNav)
 handleCurrentYear()
+btnMon.addEventListener("click", showMon)
+btnInari.addEventListener("click", showInari)
+btnSoft.addEventListener("click", showSoft)
+btnSoftTwo.addEventListener("click", showSoft2)
+btnX.addEventListener("click", removePicture)
+itemPhoto.addEventListener("mousemove", zoomImg)
+itemPhoto.addEventListener("mouseout", resetImg)
